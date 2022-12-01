572ce5bd58d21221673a6adf0a4d4357a66fe5fb
Rayyann-dev <100017396+Rayyann-dev@users.noreply.github.com>
2022-12-01 10:58:39 +0400

    generate random users first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function roll(max, min, floatflag) {
+    let r = Math.random() * (max - min) + min;
+    return floatflag ? r : Math.floor(r);
+  }
+  
+  let userName = ["Sara", "Lisa", "Raha", "Meer"];
+  
+  //ages between 12 and 64
+  //Heights between 5.1 and 6
+  
+  let user = {
+    name: userName[roll(0, userName.length)],
+    age: roll(12, 65),
+    height: roll(5.1, 6.1, 1).toFixed(1),
+  };
+  console.log(user);
+  
